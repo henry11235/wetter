a2c776335067e73694dd8431b0e664334b41ac4c
Henry Bartke <henry.bartke3@gmail.com>
2025-05-08 13:45:01 +0200

    anzeige als fenster

--- src/wetter.py
+++ src/wetter.py
@@ -1,5 +1,9 @@
 import tkinter as tk
-root = tk.Tk()  
-root.title("Wetter App")  
-root.geometry("400x300")  
+
+root = tk.Tk()
+root.title("Wetter App")
+root.geometry("400x300")
+
+# Wichtig: Diese Zeile muss am Ende stehen
+root.mainloop()
 
